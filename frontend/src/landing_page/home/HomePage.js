ddfa529a6ba68de16de610155c0b0f54aee5e59d
Devanshi-4831 <bhungaliyadevanshi318@gmail.com>
2025-05-21 15:52:58 +0530

    add new file

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -11,8 +11,8 @@ import Footer from '../Footer';
 function HomePage() {
     return ( 
         <>
-            <Navbar/>
-            <Hero/>
+            <Navbar />
+            <Hero />
             <Awards />
             <Stats />
             <Pricing />
